1f366860616a54a207dadb3dbb76b11343b269ed
Pavel Rybalko <freedom277f@gmail.com>
2020-09-27 12:26:15 +0300

    add disabled

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const startBtnRef = document.querySelector("button[data-action=start]");
-const stopBtnRef = document.querySelector("button[data-action=stop]");
+const startBtnRef = document.querySelector('button[data-action=start]');
+const stopBtnRef = document.querySelector('button[data-action=stop]');
 let intervalId = null;
 let isActive = false;
 
 const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
 ];
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-startBtnRef.addEventListener("click", changeBackground);
+startBtnRef.addEventListener('click', changeBackground);
 
 function changeBackground() {
   if (isActive) {
@@ -28,9 +28,11 @@ function changeBackground() {
       colors[randomIntegerFromInterval(0, 5)];
   }, 1000);
   isActive = true;
+  startBtnRef.disabled = true;
 }
 
-stopBtnRef.addEventListener("click", () => {
+stopBtnRef.addEventListener('click', () => {
   clearInterval(intervalId);
   isActive = false;
+  startBtnRef.disabled = false;
 });
